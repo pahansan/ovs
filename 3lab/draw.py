b0ff202506b31dc3f448ec3a777b023941e1e9db
Kulik Pavel <pashakulik58@gmail.com>
2026-03-02 22:54:25 +0700

    [lab 3] Add program to measure relation and script to draw plot

---
+++ 3lab/draw.py
@@ -0,0 +1,57 @@
+import os
+import sys
+
+import matplotlib.pyplot as plt
+
+
+def build_runtime_plots(input_dir, output_file):
+    if not os.path.isdir(input_dir):
+        print(f"Error: {input_dir} not exists")
+        return
+
+    plt.figure(figsize=(10, 7))
+
+    files = sorted([f for f in os.listdir(input_dir) if f.endswith('.txt')])
+
+    if not files:
+        print("Can't find txt files")
+        return
+
+    for filename in files:
+        m_tasks = []
+        runtime = []
+
+        file_path = os.path.join(input_dir, filename)
+        with open(file_path, 'r') as f:
+            for line in f:
+                parts = line.split()
+                if len(parts) >= 2:
+                    try:
+                        m_tasks.append(int(parts[0]))
+                        runtime.append(float(parts[1]))
+                    except ValueError:
+                        continue
+
+        data = sorted(zip(m_tasks, runtime))
+        x_vals, y_vals = zip(*data)
+
+        label_name = os.path.splitext(filename)[0]
+        plt.plot(x_vals, y_vals, marker='s', markersize=4, label=label_name)
+
+    plt.title('Зависимость t(n) времени выполнения от количества элементарных машин', fontsize=14)
+    plt.xlabel('Количество элементарных машин', fontsize=12)
+    plt.ylabel('Время выполнения, с', fontsize=12)
+
+    plt.grid(True, which='both', linestyle='--', alpha=0.5)
+    plt.legend(title="Алгоритм и параметры")
+
+    plt.tight_layout()
+
+    plt.savefig(output_file, dpi=300)
+    print(f"Plot saved: {output_file}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(f"Usage: python {sys.argv[1]} <input_dir> <output_png_name>")
+    else:
+        build_runtime_plots(sys.argv[1], sys.argv[2])
---
+++ 3lab/src/second/main.cpp
@@ -0,0 +1,108 @@
+#include <algorithm>
+#include <chrono>
+#include <cstddef>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <ostream>
+#include <vector>
+
+class PaymentMatrix {
+  size_t rows_, cols_;
+  std::vector<double> matrix;
+
+public:
+  struct Solution {
+    std::vector<double> X, Y, P, PE;
+    size_t n_iter = 0;
+    Solution(size_t n) : X(n + 1, 0), Y(n + 1, 0), P(n + 1, 0), PE(n + 1, 0) {}
+    void add_row(const PaymentMatrix &C, size_t i) {
+      P[i]++;
+      for (size_t j = 0; j < X.size(); j++)
+        Y[j] += C.elem(i, j);
+    }
+    void add_col(const PaymentMatrix &C, size_t j) {
+      PE[j]++;
+      for (size_t i = 0; i < X.size(); i++)
+        X[i] += C.elem(i, j);
+    }
+    void add_iter() { n_iter++; }
+    size_t i() {
+      return std::distance(X.begin(), std::max_element(X.begin(), X.end()));
+    }
+    size_t j() {
+      return std::distance(Y.begin(), std::min_element(Y.begin(), Y.end()));
+    }
+    void normalize(size_t n) {
+      for (int i = 0; i < X.size(); i++) {
+        P[i] /= n;
+        PE[i] /= n;
+      }
+    }
+    double cost() {
+      return (*std::max_element(X.begin(), X.end()) -
+              *std::min_element(Y.begin(), Y.end())) /
+             (n_iter * 2);
+    }
+  };
+
+  PaymentMatrix(size_t n, double c1, double c2, double c3)
+      : rows_(n + 1), cols_(n + 1), matrix(rows_ * cols_) {
+    for (size_t i = 0; i < rows_; i++)
+      for (size_t j = 0; j < cols_; j++)
+        if (i >= j)
+          elem(i, j) = j * c1 + (i - j) * c2;
+        else
+          elem(i, j) = i * c2 + (j - i) * c3;
+  }
+  double &elem(size_t i, size_t j) { return matrix[j + cols_ * i]; }
+  const double &elem(size_t i, size_t j) const { return matrix[j + cols_ * i]; }
+  size_t rows_count() { return rows_; }
+  size_t cols_count() { return cols_; }
+  friend std::ostream &operator<<(std::ostream &os, const PaymentMatrix &pm) {
+    for (size_t i = 0; i < pm.rows_; i++) {
+      for (size_t j = 0; j < pm.cols_; j++) {
+        os << std::fixed << std::setprecision(2) << std::setw(6) << std::right
+           << pm.elem(i, j);
+      }
+      os << '\n';
+    }
+    return os;
+  }
+  Solution BrownMethod(const double eps) {
+    Solution sol = Solution(rows_ - 1);
+    do {
+      size_t i = sol.i();
+      size_t j = sol.j();
+      sol.add_row(*this, i);
+      sol.add_col(*this, j);
+      sol.add_iter();
+    } while (sol.cost() * 2 >= eps);
+    sol.normalize(sol.n_iter);
+    return sol;
+  }
+};
+
+int main() {
+  size_t start_n = 10;
+  size_t step = 10;
+  size_t end_n = 100;
+  size_t n_runs = 10;
+  double eps = 0.01;
+  std::string output;
+  for (int i = start_n; i <= end_n; i += step) {
+    PaymentMatrix m(i, 1, 2, 3);
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int n = 0; n < n_runs; n++)
+      m.BrownMethod(eps);
+    auto end = std::chrono::high_resolution_clock::now();
+    size_t duration =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
+            .count();
+    double duration_in_seconds =
+        static_cast<double>(duration) / 1000000000.0 / n_runs;
+    std::cout << std::setw(6) << std::left << i << std::fixed
+              << std::setprecision(9) << duration_in_seconds << '\n';
+  }
+}
