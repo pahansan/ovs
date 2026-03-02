12ec5939e5e82f1fc4475468d1c2294c26df120d
Kulik Pavel <pashakulik58@gmail.com>
2026-03-02 22:39:34 +0700

    [lab 3] Add Brown method and tz-like output

--- 3lab/src/main.cpp
+++ 3lab/src/main.cpp
@@ -1,16 +1,53 @@
+#include <algorithm>
 #include <cstddef>
 #include <iomanip>
 #include <ios>
 #include <iostream>
+#include <iterator>
 #include <ostream>
 #include <vector>
+
 class PaymentMatrix {
   size_t rows_, cols_;
   std::vector<double> matrix;
 
 public:
+  struct Solution {
+    std::vector<double> X, Y, P, PE;
+    size_t n_iter = 0;
+    Solution(size_t n) : X(n + 1, 0), Y(n + 1, 0), P(n + 1, 0), PE(n + 1, 0) {}
+    void add_row(const PaymentMatrix &C, size_t i) {
+      P[i]++;
+      for (size_t j = 0; j < X.size(); j++)
+        Y[j] += C.elem(i, j);
+    }
+    void add_col(const PaymentMatrix &C, size_t j) {
+      PE[j]++;
+      for (size_t i = 0; i < X.size(); i++)
+        X[i] += C.elem(i, j);
+    }
+    void add_iter() { n_iter++; }
+    size_t i() {
+      return std::distance(X.begin(), std::max_element(X.begin(), X.end()));
+    }
+    size_t j() {
+      return std::distance(Y.begin(), std::min_element(Y.begin(), Y.end()));
+    }
+    void normalize(size_t n) {
+      for (int i = 0; i < X.size(); i++) {
+        P[i] /= n;
+        PE[i] /= n;
+      }
+    }
+    double cost() {
+      return (*std::max_element(X.begin(), X.end()) -
+              *std::min_element(Y.begin(), Y.end())) /
+             (n_iter * 2);
+    }
+  };
+
   PaymentMatrix(size_t n, double c1, double c2, double c3)
-      : rows_(n + 1), cols_(n + 1), matrix(std::vector<double>(rows_ * cols_)) {
+      : rows_(n + 1), cols_(n + 1), matrix(rows_ * cols_) {
     for (size_t i = 0; i < rows_; i++)
       for (size_t j = 0; j < cols_; j++)
         if (i >= j)
@@ -32,9 +69,35 @@ public:
     }
     return os;
   }
+  Solution BrownMethod(const double eps) {
+    Solution sol = Solution(rows_ - 1);
+    do {
+      size_t i = sol.i();
+      size_t j = sol.j();
+      sol.add_row(*this, i);
+      sol.add_col(*this, j);
+      sol.add_iter();
+    } while (sol.cost() * 2 >= eps);
+    sol.normalize(sol.n_iter);
+    return sol;
+  }
 };
 
 int main() {
   PaymentMatrix m(10, 1, 2, 3);
+  std::cout << "Maтpицa плaтeжeй C:\n";
   std::cout << m;
+  double eps = 0.01;
+  auto sol = m.BrownMethod(eps);
+  std::cout << "Koличecтвo итepaций l = " << sol.n_iter << '\n';
+  std::cout << "Цeнa игpы V = " << std::setprecision(3) << sol.cost() << '\n'
+            << std::setprecision(2);
+  std::cout << "Oптимaльныe cмeшaнныe cтpaтeгии ВЦ:\n";
+  for (const auto &elem : sol.P)
+    std::cout << elem << ' ';
+  std::cout << '\n';
+  std::cout << "Oптимaльныe cмeшaнныe cтpaтeгии диcпeтчepa:\n";
+  for (const auto &elem : sol.PE)
+    std::cout << elem << ' ';
+  std::cout << '\n';
 }
